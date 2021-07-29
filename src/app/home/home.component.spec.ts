218b31824ebd32dbe2e363163763e0f5563535c3
Vaddi <sv185155@ncr.com>
2021-07-29 15:15:00 +0530

    test cases updated

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,5 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { WeatherService } from '../services/weather.service';
 
 import { HomeComponent } from './home.component';
@@ -10,18 +11,30 @@ describe('HomeComponent', () => {
   let fixture: ComponentFixture<HomeComponent>;
   let weatherServiceStub;
 
+  const weatherData = [
+    {
+      cityName: "London",
+      temparature: 292.41,
+      sunsetTime: 1627502124,
+      sunriseTime: 1627445901,
+      timezone: 3600
+    }
+  ]
+
   beforeEach(async () => {
     weatherServiceStub = {
-      getDataForCities : () => {}
+      getDataForCities: () => {
+        return of(weatherData)
+      }
     }
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ],
-      providers: [{provide: WeatherService, useValue: weatherServiceStub}],
-      schemas : [
+      declarations: [HomeComponent],
+      providers: [{ provide: WeatherService, useValue: weatherServiceStub }],
+      schemas: [
         CUSTOM_ELEMENTS_SCHEMA
       ],
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -32,5 +45,12 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get the cities', (done: DoneFn) => {
+    component.cities$?.subscribe((data) => {
+      expect(data).toEqual(weatherData);
+      done();
+    })
   });
 });
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  appId: "3d8b309701a13f65b660fa2c64cdc517",
+  weatherBaseUrl: "api.openweathermap.org/data/2.5"
 };
