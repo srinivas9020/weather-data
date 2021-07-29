553faa8833b7839f0246529e23d212bfe2eb4b98
Vaddi <sv185155@ncr.com>
2021-07-29 09:47:49 +0530

    test cases updated

--- src/app/pipes/localtime-from-unix.pipe.spec.ts
+++ src/app/pipes/localtime-from-unix.pipe.spec.ts
@@ -1,8 +1,13 @@
 import { LocaltimeFromUnixPipe } from './localtime-from-unix.pipe';
 
 describe('LocaltimeFromUnixPipe', () => {
+  const pipe = new LocaltimeFromUnixPipe();
+
   it('create an instance', () => {
-    const pipe = new LocaltimeFromUnixPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should transform to date', () => {
+    expect(pipe.transform(1627445901, 3600)).toEqual(new Date(2021,6,28,10,48,21));
+  })
 });
--- src/app/pipes/localtime-from-unix.pipe.ts
+++ src/app/pipes/localtime-from-unix.pipe.ts
@@ -7,13 +7,10 @@ export class LocaltimeFromUnixPipe implements PipeTransform {
 
   transform(value: number, ...args: number[]): Date | null {
     const [timezone] = [...args];
-    if(!value && !timezone){
-      return null;
-    }
     return this.calculateLocalTime(value, timezone);
   }
 
-  calculateLocalTime(unixTime: number, timezone: number){
+  private calculateLocalTime(unixTime: number, timezone: number){
     return new Date((unixTime+timezone) * 1000);
   }
 
